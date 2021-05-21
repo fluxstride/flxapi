c1ea198b19fdecb1cfd6f8077a1668611496d375
Samuel Adekoya <65285073+fluxstride@users.noreply.github.com>
2021-05-21 15:50:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 let express = require("express");
 let { MongoClient, ObjectId } = require("mongodb");
-let connectionString = process.env.MONGODB_URI;
+let connectionString = "mongodb+srv://admin:admin@apis.eqlrf.mongodb.net/api1?retryWrites=true&w=majority" ;
 
 
 let app = express();
